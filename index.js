2e631c4e74bd5fbdc3160d7dcfc4e0e66dd8c072
jagedos <jagedosusrecomm@gmail.com>
2024-06-27 23:19:31 +0300

    concatenation and interpolation

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = "Ayako Ondiege";
+
+// const welcomeMessage = "welcome to Flatbook, ";
+
+const welcomeMessage =`'Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, ' + `${currentUser.slice(0,1)}!`;
+
+console.log(shortGreeting)
+
+
+
